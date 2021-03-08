27f1c39c83233d7746bf8b618291752df91c6087
salboro <salboro121@gmail.com>
2021-03-08 16:27:33 +0700

    moved location logic to WeatherAppLocationService.kt

---
+++ app/src/main/java/com/example/weatherapp/data/WeatherAppLocationService.kt
@@ -0,0 +1,139 @@
+package com.example.weatherapp.data
+
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
+import android.os.Looper
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import java.util.concurrent.TimeUnit
+
+
+class WeatherAppLocationService(
+    private val context: Context,
+    private val activity: Activity,
+    private val bindCitiesList: (location: Location) -> Unit
+) {
+
+    private val REQUEST_LOCATION_PERMISSION = 1
+
+    private var isPermissionChecked: Boolean = false
+
+    private lateinit var locationRequest: LocationRequest
+
+    private lateinit var locationCallback: LocationCallback
+
+    private var locationService: FusedLocationProviderClient = FusedLocationProviderClient(context)
+
+
+    suspend fun getLastLocation() {
+        checkPermission()
+        if (isPermissionChecked) {
+            if (isLocationEnable()) {
+                locationService.lastLocation.addOnSuccessListener { location ->
+                    if (location != null) {
+                        bindCitiesList(location)
+                    } else {
+                        requestNewLocation()
+                    }
+                }
+            } else {
+                Toast.makeText(
+                    context,
+                    "You have trouble with connection.",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+    }
+
+    fun requestNewLocation() {
+        locationRequest = LocationRequest.create().apply {
+            interval = TimeUnit.SECONDS.toMillis(60)
+            fastestInterval = TimeUnit.SECONDS.toMillis(5)
+            maxWaitTime = TimeUnit.MINUTES.toMillis(2)
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                super.onLocationResult(locationResult)
+                checkPermission()
+                locationService.lastLocation.addOnSuccessListener { location ->
+                    if (location != null) {
+                        bindCitiesList(location)
+                    } else {
+                        requestNewLocation()
+                    }
+                }
+            }
+        }
+
+        checkPermission()
+        locationService.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            Looper.getMainLooper()
+        )
+
+    }
+
+
+    private fun checkPermission() {
+        if (ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            enablePermission()
+        } else {
+            isPermissionChecked = true
+        }
+    }
+
+    private fun isLocationEnable(): Boolean {
+        val locationManager: LocationManager =
+            context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || (locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        ))
+    }
+
+    private fun isPermissionGranted(): Boolean {
+        return (ContextCompat.checkSelfPermission(
+            context,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED) &&
+                (ContextCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED)
+    }
+
+
+    fun enablePermission() {
+        if (isPermissionGranted()) {
+            isPermissionChecked = true
+        } else {
+            ActivityCompat.requestPermissions(
+                activity,
+                arrayOf<String>(
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ),
+                REQUEST_LOCATION_PERMISSION
+            )
+        }
+    }
+}
