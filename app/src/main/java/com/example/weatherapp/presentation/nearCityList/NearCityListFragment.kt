fa37a56208f660f440cc4f1867e0fea44bd491c0
salboro <salboro121@gmail.com>
2021-03-09 20:00:44 +0700

    another edits

--- app/src/main/java/com/example/weatherapp/presentation/nearCityList/NearCityListFragment.kt
+++ app/src/main/java/com/example/weatherapp/presentation/nearCityList/NearCityListFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -88,5 +89,10 @@ class NearCityListFragment : Fragment() {
                 weatherAppLocationService.enablePermission()
             }
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(context, "Try swipe right", Toast.LENGTH_SHORT).show()
     }
 }
